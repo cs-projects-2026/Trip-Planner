3235a4704c7d05b4f1376f035c0bbf1c74f7bc0f
David <cheungdavid04@gmail.com>
2026-06-02 13:00:26 +0800

    completed timetable.h and timetable.cpp

--- timetable.cpp
+++ timetable.cpp
@@ -1,1 +1,55 @@
 #include "all_includes.h"
+
+//helper function, checking if dates are valid 
+static bool isValidDate(const Date& date){
+    if(date.month < 1 || date.month > 12 || date.day < 1 || date.day > 31)
+        return false; 
+    int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; //index represents the month, ignore 0, since month 0 does not exist, eg. index 1 is Jan with 31 days 
+
+    //checking for leap year
+    if(date.month == 2){
+        bool isLeap = (date.year % 4 == 0 && date.year % 100 != 0) || (date.year % 400 == 0);
+        if(isLeap) 
+            daysInMonth[2] = 29; 
+    }
+    
+    return date.day <= daysInMonth[date.month];
+}
+
+Timetable::Timetable(int start, int end){
+    startYear = start; 
+    endYear = end; 
+}
+
+void Timetable::setAvailability(const Date& date, bool availablity){
+    if(!isValidDate(date) || date.year < startYear || date.year > endYear) 
+        return; 
+    if(availablity){
+        cout << "[-] removed " << date.day << "/" << date.month << "/" << date.year << "from unavailableDates set" << endl; 
+        unavailableDates.erase(date);
+    }
+    if(!availablity){
+        cout << "[+] added " << date.day << "/" << date.month << "/" << date.year << "to unavailableDates set" << endl;
+        unavailableDates.insert(date); 
+    }
+}
+
+bool Timetable::getAvailability(const Date& date) const{ 
+    if(!isValidDate(date) || date.year < startYear || date.year > endYear){
+        cout << "[!] getAvailability() | " << date.day << "/" << date.month << "/" << date.year << " | returned false by error, invalid date or year out of bounds" <<endl; 
+        return false; 
+    }
+    return unavailableDates.find(date) == unavailableDates.end();
+    /* 
+    if found (meaning it's an unavailable date) then unavailableDates.find(date) != unavailableDates.end() returns false (unavaiable date exists)
+    if not found (unavailable date doesn't exist) then unavailableDates.find(date) returns .end() so unavailableDates.find(date) != unavailableDates.end() returns true
+    */
+}
+
+int Timetable::getStartYear() const{
+    return startYear;
+}
+
+int Timetable::getEndYear() const{
+    return endYear;
+}
--- timetable.h
+++ timetable.h
@@ -1,15 +1,51 @@
 #ifndef TIMETABLE_H
 #define TIMETABLE_H
+#include <set> 
 #include "all_includes.h"
+
+//Date object 
+struct Date {
+    int year; 
+    int month; 
+    int day; 
+
+    /*
+    operator< executes only when it detects a comparison between two date objects
+    if d1 and d2 are both Date objects 
+    d1 < d2 is the same as d1.operator<(d2) 
+    */
+
+    bool operator< (const Date& other) const { //this function calculates and returns d1 < d2
+        if(year != other.year) 
+            return year < other.year;
+        if(month != other.month)
+            return month < other.month;
+        return day < other.day; 
+    }
+
+    /*
+    similar to above, operator== executes only when it detects a comparison between two date objects
+    d1 == d2 is same as d1.operator==(d2)
+    */
+    bool operator==(const Date& other) const { //returns and calculates d1 == d2 
+        return year == other.year && month == other.month && day == other.day; 
+    }
+
+};
 
 class Timetable{ 
     private: 
         int startYear;
         int endYear;
-        bool timetable[5][7];
-
+        std::set<Date> unavailableDates; 
+        //only stores unavailable dates 
+        
     public:
-        Timetable();
+        Timetable(int start, int end);
+        void setAvailability(const Date& date, bool availability);
+        bool getAvailability(const Date& date) const; 
+        int getStartYear() const; 
+        int getEndYear() const; 
 };
 
 #endif
